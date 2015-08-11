98fde78b0f724222e5c927b5fe8b2e7ebef013fa
zhzhang2012 <zhzhangtony@gmail.com>
2015-08-11 23:21:34 +0800

    Index page layout migrated. Architecture revised

--- webapp/app.js
+++ webapp/app.js
@@ -2,4 +2,5 @@
 
 angular.module('Center', ['ngCookies', 'ngResource',
                           'Center.index.controller',
+                          'Center.headers.controller',
                           'Center.project.controller']);
---
+++ webapp/modules/headers/headers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('Center.headers.controller', [])
+    .controller('TopHeaderController', ['$scope', function($scope) {
+
+    }])
+    .controller('BottomHeaderController', ['$scope', function($scope) {
+
+    }]);
